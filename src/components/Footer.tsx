c52e389387001c1900186495927d12ec015b5f9e
Raulik <raul.hajiyev95@gmail.com>
2024-05-10 01:00:29 -0500

    create footer layout

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,19 @@
-import { Heading, VStack } from '@chakra-ui/react'
-import React from 'react'
+import { Flex, Box, Text, VStack, Center} from '@chakra-ui/react'
+
 
 const Footer = () => {
   return (
-    <VStack bg="blue" h="15vh" margin="0"> 
-      <Heading> FOOTER</Heading>
+    <VStack bg="#47B8B1" h="15vh" margin="0" alignContent="space-between" alignItems="space-between" padding="40px" > 
+      
+      <Flex justifyContent="space-between">
+         <Box boxSize="40px" bg="white">Logo </Box>
+      <Box>Address </Box>
+      </Flex>
+      <Center>
+         <Text> 
+         © 2024 SEA LLC. All Rights Reserved.
+         </Text>
+      </Center>
     </VStack>
   )
 }
